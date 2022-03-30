2b5cf18f7ad57f7c0577462d12d3747afe096880
Jose Romary <joseromarybrandao@gmail.com>
2022-03-30 20:14:37 -0300

    validação de dados formulário criação usuario

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { Flex, Button, Stack } from "@chakra-ui/react";
 import { resolve } from "path";
-import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "../components/Form/input.";
+import { SubmitHandler, useForm } from "react-hook-form";
 import * as yup from 'yup'
 import {yupResolver} from '@hookform/resolvers/yup'
 
